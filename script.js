3cab9564f7a41c4cb91cbf050e82314a51210353
Paulina Lei <paulina.y.lei@gmail.com>
2017-07-17 07:45:06 +0100

    will this work for https

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-var URL2 = 'http://api.population.io/1.0/population/2010/United%20States/';
+var URL2 = '//api.population.io/1.0/population/2010/United%20States/';
 var URL = 'https://api.github.com/emojis';
 xhr.onreadystatechange = function () {
   console.log('hello');
