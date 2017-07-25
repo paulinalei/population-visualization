e9d966e97b79a6e555dc41c705641407b269992f
Paulina Lei <paulina.y.lei@gmail.com>
2017-07-25 11:48:55 -0700

    refactored code

--- script.js
+++ script.js
@@ -19,22 +19,39 @@ xhr.onload = function () {
     console.log(femalePopulation);
     if (femalePopulation.length > 0) {
       console.log('we in here');
-      d3.select(".data")
+      var width = 400;
+      var barHeight = 20;
+
+      var x = d3.scaleLinear()
+        .domain([0, d3.max(femalePopulation)])
+        .range([0, width]);
+
+      var chart = d3.select('.chart')
+        .attr('width', width)
+        .attr('height', barHeight * femalePopulation.length);
+
+      var bar = chart.selectAll('g')
         .data(femalePopulation)
         .enter()
-        .append("svg")
-        .style("width", function(d) { return d/10000 + "px"; })
-        .style('background-color', function (d) { return 'black'; })
-        // add mouseover effect to change background color to black
-        .on('mouseover', function() {
-          d3.select(this)
-            .style('background-color','red')
+          .append('g')
+          .attr('transform', function(d, i) {
+            return 'translate(0,' + i * barHeight + ')';
+          });
+
+      bar.append('rect')
+        .attr('width', x)
+        .attr('height', barHeight - 1);
+
+      bar.append('text')
+        .attr('x', function(d) {
+          return x(d) - 3;
         })
-        .on('mouseout', function () {
-          d3.select(this)
-            .style('background-color', function (d) { return 'black'; })
-        })
-        .text(function(d) { return d.females; });
+        .attr('y', barHeight/2)
+        .attr('dy', '.35em')
+        .text(function(d) {
+          return d;
+        });
+
     }
   }
 }
